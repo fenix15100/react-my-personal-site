9f4157c2e4ceed521596c020861c87323ab2264b
fenix15100 <francisco.camacho15@gmail.com>
2020-01-30 13:02:51 +0100

    Add Navbar component in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
+
+
 function App() {
   return (
-    <h1>its,Works</h1>
+    <Navbar className="header-bg" variant="light"  expand="lg">
+      <Navbar.Brand href="#home">MyPersonalSite</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto myNav">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#link">Link</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
