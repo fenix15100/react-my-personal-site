32a74df618ad3914aae172d0e2a9904ccf6d599b
fenix15100 <francisco.camacho15@gmail.com>
2020-01-30 14:50:03 +0100

    i

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container'
 
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -19,11 +20,11 @@ function App() {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="./react-my-personal-site/">Home</Nav.Link>
-          <Nav.Link href="./react-my-personal-site/resume">Resume</Nav.Link>
-          <Nav.Link href="./react-my-personal-site/about-me">AboutMe</Nav.Link>
-          <Nav.Link href="./react-my-personal-site/projects">Projects</Nav.Link>
-          <Nav.Link href="./react-my-personal-site/contact">Contact</Nav.Link>  
+          <Nav.Link href="/react-my-personal-site/">Home</Nav.Link>
+          <Nav.Link href="/react-my-personal-site/resume">Resume</Nav.Link>
+          <Nav.Link href="/react-my-personal-site/about-me">AboutMe</Nav.Link>
+          <Nav.Link href="/react-my-personal-site/projects">Projects</Nav.Link>
+          <Nav.Link href="/react-my-personal-site/contact">Contact</Nav.Link>  
         </Nav>
       </Navbar.Collapse>
     </Navbar>
