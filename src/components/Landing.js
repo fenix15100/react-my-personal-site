618b42c8060a9415295aa45f9f7913b0ca4d2cca
fenix15100 <francisco.camacho15@gmail.com>
2020-01-31 16:00:47 +0100

    yep

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,40 +2,29 @@ import React from 'react';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
-import avatar from './assets/avatar.png'
+import avatar from './assets/avatar2.jpg'
 
 import './css/Landing.css'
 const Landing = () => {
     return (
             <Row className="landing-grid text-center">
                 <Col sm={12}>
-                    <img 
-                        src={avatar} 
-                        alt="avatar"
-                        className="avatar-img img-fluid"
-                        
-                    />
-                </Col>
-                <Col sm={12}>
-                    <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
-                     <div className="banner-text">
-                        <h1>Francisco José Camacho Maya</h1>    
-                        <h1>Full Stack Developer</h1>
-                        <h1>"reCoding minds"</h1>
-                        <hr/>
-                        <div className="social-links">
-                            <a href="https://www.linkedin.com/in/franciscojosecamacho/" target="_blank" rel="noopener noreferrer">
-                                <i className="fa fa-linkedin-square" aria-hidden="true"></i>
-                            </a>
-                            <a href="https://github.com/fenix15100" target="_blank" rel="noopener noreferrer">
-                                <i className="fa fa-github-square" aria-hidden="true"></i>
-                            </a>
-                            <a href="https://twitter.com/fenix15100" target="_blank" rel="noopener noreferrer">
-                                <i className="fa fa-twitter-square" aria-hidden="true"></i>
-                            </a>
+                        <div className="card">
+                            <div className="card__image-container">
+                                <img className="card__image" src={avatar} alt="me"/>
+                            </div>
+      
+                            <svg className="card__svg" viewBox="0 0 800 500">
+
+                                <path d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400 L 800 500 L 0 500" stroke="transparent" fill="#333"/>
+                                <path className="card__line" d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400" stroke="pink" strokeWidth="3" fill="transparent"/>
+                            </svg>
+    
+                            <div className="card__content">
+                            <h4 className="card__title">Francisco José Camacho Maya</h4>
+                            <p>{`{Another Full stack developer}`}</p>
+                            </div>
                         </div>
-                    </div>
-                    <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
                 </Col>           
             </Row>    
     );
