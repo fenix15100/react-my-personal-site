73f10ed47b37c09196fbca3b2d8c4750cf4ec591
fenix15100 <francisco.camacho15@gmail.com>
2020-02-04 17:49:22 +0100

    dsds

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React,{useState} from 'react';
+import Tabs from 'react-bootstrap/Tabs';
+import Tab from 'react-bootstrap/Tab';
+import { Row,Col } from 'react-bootstrap';
+// TAGS import Badge from 'react-bootstrap/Badge'
 const Projects = () => {
+
+    const [key, setKey] = useState('home');
+
     return (
-        <h1>Projects</h1>
+        <Row>
+            <Col>
+                <Tabs id="controlled-tab-example" activeKey={key} onSelect={k => setKey(k)}>
+                    <Tab eventKey="home" title="Home">
+                        <h1>python</h1>
+                    </Tab>
+                    <Tab eventKey="profile" title="Profile">
+                        <h1>python1</h1>
+                    </Tab>
+                    <Tab eventKey="contact" title="Contact">
+                        <h1>python2</h1>
+                    </Tab>
+                </Tabs>
+            </Col>
+
+
+        </Row>
+        
     );
 }
 
