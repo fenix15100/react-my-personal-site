02fc823797370f0f36ea5fa1e82946ce8f58347e
fenix15100 <francisco.camacho15@gmail.com>
2020-02-04 13:51:42 +0100

    controls carrousel

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -3,16 +3,22 @@ import Carousel from 'react-bootstrap/Carousel'
 
 const Quotes = () => {
     return (
-      <Carousel controls={true} indicators={false} interval={3500} className="quotes">
+      <Carousel 
+        controls={true} 
+        indicators={false} 
+        interval={3500}
+        prevIcon={<span><i className="fa fa-angle-left" aria-hidden="true"></i></span>}
+        nextIcon={<span><i className="fa fa-angle-right" aria-hidden="true"></i></span>}
+        >
+        
         <Carousel.Item>
           <h2>Hi, I'am Francisco A Full Stack Developer</h2>
         </Carousel.Item>
         <Carousel.Item>
           <h2>LORE</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-
         </Carousel.Item>
-  </Carousel>
+      </Carousel>
         
     );
 }
