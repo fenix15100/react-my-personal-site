49e3de5f71247855a1a1a2309d849b213d4d6b8f
fenix15100 <francisco.camacho15@gmail.com>
2020-02-05 13:22:59 +0100

    compose project.js
    falta implementar un dialog y luego enpasularlo todo en un componente carta

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,196 +1,122 @@
-import React,{useState} from 'react';
-import Tabs from 'react-bootstrap/Tabs';
-import Tab from 'react-bootstrap/Tab';
+import React from 'react';
 import { Row,Col } from 'react-bootstrap';
-import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-import './css/Projects.css';
+import Card from 'react-bootstrap/Card';
+import Badge from 'react-bootstrap/Badge'
+import './css/Projects.css'
 // TAGS import Badge from 'react-bootstrap/Badge'
 const Projects = () => {
 
-    const [key, setKey] = useState('home');
+    
 
     return (
-        <Row className="projects-grid">
-                <Col>
-                    <Tabs  className="justify-content-center projects-tab-pane" id="controlled-tab" activeKey={key} onSelect={k => setKey(k)}>
-                        <Tab  className="projects-tab-pane-content" eventKey="home" title="Home">
-                            <Card>
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                    </Card.Text>
-                                    <div className="card-actions">
-                                        <Button variant="primary">Go somewhere</Button>
-                                        <Button variant="primary">Go somewhere</Button>
-                                        <Button variant="primary">Go somewhere</Button>
-                                    </div>
-                                </Card.Body>
-                            </Card>
+        <Row>
+            <Col sm={12} className="projects-grid">
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg" />
+                    <Card.Body>
+                        <Card.Title>
+                            Card Title&nbsp; 
+                            <Badge pill variant="primary">react</Badge>
+                            <Badge pill variant="warning">python</Badge>
+                        </Card.Title>
+                        <Card.Text>
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                        </Card.Text>
+                        <div className="card-actions">
+                            <Button size="sm" variant="outline-info">Github</Button>
+                            <Button size="sm" variant="outline-info">More Info</Button>
+                            <Button size="sm" variant="outline-info">Live Demo</Button>
+                        </div>
+                    </Card.Body>
+                </Card>
 
-                            <Card>
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                    </Card.Text>
-                                    <div className="card-actions">
-                                        <Button variant="primary">Go somewhere</Button>
-                                        <Button variant="primary">Go somewhere</Button>
-                                        <Button variant="primary">Go somewhere</Button>
-                                    </div>
-                                </Card.Body>
-                            </Card>
-                        </Tab>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg" />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                        </Card.Text>
+                        <div className="card-actions">
+                            <Button size="sm" variant="outline-info">Github</Button>
+                            <Button size="sm"variant="outline-info">More Info</Button>
+                            <Button size="sm" variant="outline-info">Live Demo</Button>
+                        </div>
+                    </Card.Body>
+                </Card>
 
-                        <Tab  className="projects-tab-pane-content" eventKey="home" title="Home">
-                            <Card>
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                    </Card.Text>
-                                    <div className="card-actions">
-                                        <Button variant="primary">Go somewhere</Button>
-                                        <Button variant="primary">Go somewhere</Button>
-                                        <Button variant="primary">Go somewhere</Button>
-                                    </div>
-                                </Card.Body>
-                            </Card>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg" />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                        </Card.Text>
+                        <div className="card-actions">
+                            <Button size="sm" variant="outline-info">Github</Button>
+                            <Button size="sm"variant="outline-info">More Info</Button>
+                            <Button size="sm" variant="outline-info">Live Demo</Button>
+                        </div>
+                    </Card.Body>
+                </Card>
 
-                            <Card>
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                    </Card.Text>
-                                    <div className="card-actions">
-                                        <Button variant="primary">Go somewhere</Button>
-                                        <Button variant="primary">Go somewhere</Button>
-                                        <Button variant="primary">Go somewhere</Button>
-                                    </div>
-                                </Card.Body>
-                            </Card>
-                        </Tab>
 
-                        <Tab  className="projects-tab-pane-content" eventKey="home" title="Home">
-                            <Card>
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                    </Card.Text>
-                                    <div className="card-actions">
-                                        <Button variant="primary">Go somewhere</Button>
-                                        <Button variant="primary">Go somewhere</Button>
-                                        <Button variant="primary">Go somewhere</Button>
-                                    </div>
-                                </Card.Body>
-                            </Card>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg" />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                        </Card.Text>
+                        <div className="card-actions">
+                            <Button size="sm" variant="outline-info">Github</Button>
+                            <Button size="sm"variant="outline-info">More Info</Button>
+                            <Button size="sm" variant="outline-info">Live Demo</Button>
+                        </div>
+                    </Card.Body>
+                </Card>
 
-                            <Card>
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                    </Card.Text>
-                                    <div className="card-actions">
-                                        <Button variant="primary">Go somewhere</Button>
-                                        <Button variant="primary">Go somewhere</Button>
-                                        <Button variant="primary">Go somewhere</Button>
-                                    </div>
-                                </Card.Body>
-                            </Card>
-                        </Tab>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg" />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                        </Card.Text>
+                        <div className="card-actions">
+                            <Button size="sm" variant="outline-info">Github</Button>
+                            <Button size="sm"variant="outline-info">More Info</Button>
+                            <Button size="sm" variant="outline-info">Live Demo</Button>
+                        </div>
+                    </Card.Body>
+                </Card>
+                
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="https://www.ismedioambiente.com/wp-content/uploads/2019/04/Convenio-Europeo-del-Paisaje.jpg" />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                        </Card.Text>
+                        <div className="card-actions">
+                            <Button size="sm" variant="outline-info">Github</Button>
+                            <Button size="sm"variant="outline-info">More Info</Button>
+                            <Button size="sm" variant="outline-info">Live Demo</Button>
+                        </div>
+                    </Card.Body>
+                </Card>
 
-                        <Tab  className="projects-tab-pane-content" eventKey="python" title="python">
-                            <Card>
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                    </Card.Text>
-                                    <div className="card-actions">
-                                        <Button variant="primary">Go somewhere</Button>
-                                        <Button variant="primary">Go somewhere</Button>
-                                        <Button variant="primary">Go somewhere</Button>
-                                    </div>
-                                </Card.Body>
-                            </Card>
-
-                            <Card>
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                    </Card.Text>
-                                    <div className="card-actions">
-                                        <Button variant="primary">Go somewhere</Button>
-                                        <Button variant="primary">Go somewhere</Button>
-                                        <Button variant="primary">Go somewhere</Button>
-                                    </div>
-                                </Card.Body>
-                            </Card>
-
-                            <Card>
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                    </Card.Text>
-                                    <div className="card-actions">
-                                        <Button variant="primary">Go somewhere</Button>
-                                        <Button variant="primary">Go somewhere</Button>
-                                        <Button variant="primary">Go somewhere</Button>
-                                    </div>
-                                </Card.Body>
-                            </Card>
-
-                            <Card>
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                    </Card.Text>
-                                    <div className="card-actions">
-                                        <Button variant="primary">Go somewhere</Button>
-                                        <Button variant="primary">Go somewhere</Button>
-                                        <Button variant="primary">Go somewhere</Button>
-                                    </div>
-                                </Card.Body>
-                            </Card>
-
-                            <Card>
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                    </Card.Text>
-                                    <div className="card-actions">
-                                        <Button variant="primary">Go somewhere</Button>
-                                        <Button variant="primary">Go somewhere</Button>
-                                        <Button variant="primary">Go somewhere</Button>
-                                    </div>
-                                </Card.Body>
-                            </Card>
-                        </Tab>
-                    </Tabs>
             </Col>
-
-
         </Row>
+                
+         
         
     );
 }
