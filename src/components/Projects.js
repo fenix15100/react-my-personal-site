8630fe03066ee49dc7a1a16129e4c37509c5af93
fenix15100 <francisco.camacho15@gmail.com>
2020-02-05 10:03:38 +0100

    dd

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,25 +2,41 @@ import React,{useState} from 'react';
 import Tabs from 'react-bootstrap/Tabs';
 import Tab from 'react-bootstrap/Tab';
 import { Row,Col } from 'react-bootstrap';
+import './css/Projects.css';
 // TAGS import Badge from 'react-bootstrap/Badge'
 const Projects = () => {
 
     const [key, setKey] = useState('home');
 
     return (
-        <Row>
+        <Row className="projects-grid">
             <Col>
-                <Tabs id="controlled-tab-example" activeKey={key} onSelect={k => setKey(k)}>
-                    <Tab eventKey="home" title="Home">
-                        <h1>python</h1>
-                    </Tab>
-                    <Tab eventKey="profile" title="Profile">
-                        <h1>python1</h1>
-                    </Tab>
-                    <Tab eventKey="contact" title="Contact">
-                        <h1>python2</h1>
-                    </Tab>
-                </Tabs>
+                    <Tabs  className="justify-content-center projects-tab-pane" id="controlled-tab" activeKey={key} onSelect={k => setKey(k)}>
+                        <Tab eventKey="home" title="Home">
+                            <h1>python</h1>
+                        </Tab>
+                        <Tab eventKey="profile" title="Profile">
+                            <h1>python1</h1>
+                        </Tab>
+                        <Tab eventKey="contact" title="Contact">
+                            <h1>python2</h1>
+                        </Tab>
+                        <Tab eventKey="contacff" title="Contact">
+                            <h1>python2</h1>
+                        </Tab>
+                        <Tab eventKey="contacff" title="Contact">
+                            <h1>python2</h1>
+                        </Tab>
+                        <Tab eventKey="contacff" title="Contact">
+                            <h1>python2</h1>
+                        </Tab>
+                        <Tab eventKey="contacff" title="Contact">
+                            <h1>python2</h1>
+                        </Tab>
+                        <Tab eventKey="contacff" title="Contact">
+                            <h1>python2</h1>
+                        </Tab>
+                    </Tabs>
             </Col>
 
 
