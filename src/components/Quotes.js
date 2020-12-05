2fb72180ab8253d5a26b4983937cd5433d3225f1
fenix15100 <francisco.camacho15@gmail.com>
2020-12-05 14:26:30 +0100

    changue carusel

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -11,32 +11,41 @@ const Quotes = () => {
         indicators={false} 
         interval={4000}
         >
-        <Carousel.Item>
-          <h2>Hi, I'am Francisco</h2>
-          <br></br>
-          <p>A Backend Developer (Python/PHP/Node.js &#10084;)</p>
-        </Carousel.Item>
-        <Carousel.Item>
-          <h2>Infrastructure Devops</h2>
-          <br></br>
-          <p>Vmware SDK Developer</p>
-        </Carousel.Item>
-        <Carousel.Item>
-          <h2>Automation & SysAdmin</h2>
-          <br></br>
-          <p>Scripting [Python,Bash,Powershell]</p>
-        </Carousel.Item>
-        <Carousel.Item>
-          <h2>Frontend Skills</h2>
-          <br></br>
-          <p>I can also play against frontend developers :)</p>
-        </Carousel.Item>
-        <Carousel.Item>
-          <h2>I do more things ....</h2>
-          <br></br>
-          <p><Nav.Link href="/react-my-personal-site/resume">Resume Page</Nav.Link></p>
-        </Carousel.Item>
-        
+          <Carousel.Item>
+              <div className="caption-carousel">
+                <h2>Hi, I'am Francisco</h2>
+                
+                <p>A Backend Developer (Python/PHP/Node.js &#10084;)</p>
+              </div>   
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <div className="caption-carousel">
+              <h2>Infrastructure Devops</h2>
+              <p>Vmware SDK Developer</p>
+            </div>
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <div className="caption-carousel">
+              <h2>Automation</h2>
+              
+              <p>Scripting [Python,Bash,Powershell]</p>
+            </div>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className="caption-carousel">
+              <h2>Frontend Skills</h2>
+              
+              <p>I can also play against frontend developers :)</p>
+            </div>
+          </Carousel.Item>
+          <Carousel.Item>
+          <div className="caption-carousel">
+            <h2>I do more things ....</h2>
+            <Nav.Link href="/react-my-personal-site/resume">Resume Page</Nav.Link>
+          </div>  
+          </Carousel.Item>  
       </Carousel>
         
     );
